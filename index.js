3c7441d656795d6931a1229301f1af7d15d82524
Wilfred <wilfredbayudan@gmail.com>
2021-08-11 22:51:32 -1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+const findMatching = (drivers, name) => {
+  return drivers.filter(driver => {
+    return (driver.toLowerCase() === name.toLowerCase())
+  })
+}
+
+const fuzzyMatch = (drivers, letters) => {
+  return drivers.filter(driver => {
+    let i = 0;
+    let matchStatus = true;
+    do {
+      if (driver[i] !== letters[i]) {
+        matchStatus = false;
+      }
+      i++;
+    }
+    while (i < letters.length && matchStatus === true);
+    return matchStatus;
+  })
+}
+
+const matchName = (driverObj, string) => {
+  return driverObj.filter(driver => {
+    return driver.name === string;
+  })
+}
